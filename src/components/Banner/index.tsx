12c551f04486d3f1fd335bf8326cacd4c3c6371a
MasaharuMurakami <89374344+masaharu-murakami@users.noreply.github.com>
2024-10-18 22:08:33 +0900

    fix :indexのpropsの渡し方

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,13 +1,13 @@
 import { useProps } from "./useProps";
 import { Layout } from "./Layout";
 
-export const Banner = () => {
-  return <Layout {...useProps}/>
-};
-
 // export const Banner = () => {
-//   const fetchUrl = "your-fetch-url-here"; // 適切なfetchUrlを指定してください
-//   const props = useProps(fetchUrl);
-
-//   return <Layout {...props} />;
+//   return <Layout {...useProps}/>
 // };
+
+export const Banner = () => {
+  const fetchUrl = "requests.fetchTrending"; // 適切なfetchUrlを指定してください
+  const props = useProps(fetchUrl);
+
+  return <Layout {...props} />;
+};
